49118466420758488b7164e3b1632fb20fe27cb9
Julio <jurodriguezf@unal.edu.com>
2021-04-14 21:24:23 -0500

    Fixes, code organizing, protocol packet sniffing

--- sniffer.py
+++ sniffer.py
@@ -1,3 +1,6 @@
+"""Code by Julio Rodriguez
+"""
+
 from scapy.all import * # Sniffing functions
 import sys              # Safely terminating the program
 
@@ -12,7 +15,6 @@ def detailedPacket(interface, packetNumber):
     try:
         a = sniff(count = packetNumber, iface = interfaceName, prn = lambda x: x.show())
         print(a)
-        print('{} packets sniffed successfully.'.format(packetNumber))
         return
     except:
         sys.exit('No interface found, make sure you typed the name correctly.\n')
@@ -22,14 +24,27 @@ def summarizedPacket(interface, packetNumber):
     try:
         a = sniff(count = packetNumber, iface = interfaceName, prn = lambda x: x.summary())
         print(a)
-        print('{} packets sniffed successfully.'.format(packetNumber))
         return
     except:
         sys.exit('No interface found, make sure you typed the name correctly.\n')
 
-# Print packet of a specific layer
-def layerPacket(interface, packetNumber, layer):
-    pass
+# Print ALL info of packet of a specific protocol
+def detailedProtocolPacket(interface, packetNumber, protocol):
+    try:
+        a = sniff(filter = protocol, count = packetNumber, iface = interfaceName, prn = lambda x: x.show())
+        print(a)
+        return
+    except:
+        sys.exit('No interface found, make sure you typed the name correctly.\n')
+
+# Print summarized info of packet of a specific protocol
+def summarizedProtocolPacket(interface, packetNumber, protocol):
+    try:
+        a = sniff(filter = protocol, count = packetNumber, iface = interfaceName, prn = lambda x: x.summary())
+        print(a)
+        return
+    except:
+        sys.exit('Unexpected error. Make sure the protocol is compatible with your network interface.\n')
 
 # -------------------------------------------------------------------------------------
 
@@ -40,10 +55,9 @@ def layerPacket(interface, packetNumber, layer):
 # Show main menu
 def showMenu():
     print('''\nThese are the available options:
-    1. Sniff all packets
-    2. Sniff packets of a specific layer (data link, transportation, application)
-    3. 
-    4. Exit
+    1. Sniff all packets (any protocol)
+    2. Sniff packets of a specific protocol
+    3. Exit
     ''')
     return
 
@@ -71,13 +85,83 @@ def integerInput(message):
 
 # -------------------------------------------------------------------------------------
 
-# Main
-if __name__ == '__main__':
-    print('---Welcome to the Python Sniffer program---\n')
-    
-    # Ask for the interface name
-    interfaceName = inputInterfaceName()
+# -----------------
+#  MAIN FUNCTIONS -
+# -----------------
 
+def sniffedPackets():
+    while True:
+        choice = input('\nDo you want to see all the information about each packet? (y,n): ')
+        # All the information
+        if choice in {'y', 'Y', 'yes', 'Yes'}:
+            choice = input('\nThe current default packet number is 100, would you like to change it? (y,n): ')
+            if choice in {'y', 'Y', 'yes', 'Yes'}:
+                packetNumber = integerInput('Enter the amount of packets to be sniffed: ')
+                detailedPacket(interfaceName, packetNumber)
+            else:
+                packetNumber = 100
+                detailedPacket(interfaceName, packetNumber)
+            break
+        # Summarized information
+        elif choice in {'n', 'N', 'no', 'No'}:
+            choice = input('\nThe current default packet number is 100, would you like to change it? (y,n): ')
+            if choice in {'y', 'Y', 'yes', 'Yes'}:
+                packetNumber = integerInput('Enter the amount of packets to be sniffed: ')
+                summarizedPacket(interfaceName, packetNumber)
+            else:
+                packetNumber = 100
+                summarizedPacket(interfaceName, packetNumber)
+            break
+        # Invalid option
+        else:
+            print('Invalid option\n')
+
+def protocolPackets():
+    while True:
+        print('''\nSupported protocols:
+        Ethernet (ether)
+        Wireless LAN (wlan)
+        Internet protocol (ip)
+        IPv6 (ip6)
+        Address Resolution Protocol (arp)
+        Reverse ARP (rarp)
+        Transmission Control Protocol (tcp)
+        User Datagram Protocol (udp)
+        Internet Control Message Protocol (icmp)
+        ''')
+
+        protocol = input('Enter the desired protocol to be filtered: ')
+
+        if protocol not in {'ether', 'wlan', 'ip', 'ip6', 'arp', 'rarp', 'tcp', 'udp', 'icmp'}:
+            print('Invalid protocol\n')
+            continue
+        else:
+            choice = input('\nDo you want to see all the information about each packet? (y,n): ')
+            # All the information
+            if choice in {'y', 'Y', 'yes', 'Yes'}:
+                choice = input('\nThe current default packet number is 100, would you like to change it? (y,n): ')
+                if choice in {'y', 'Y', 'yes', 'Yes'}:
+                    packetNumber = integerInput('Enter the amount of packets to be sniffed: ')
+                    detailedProtocolPacket(interfaceName, packetNumber, protocol)
+                else:
+                    packetNumber = 100
+                    detailedProtocolPacket(interfaceName, packetNumber, protocol)
+                break
+            # Summarized information
+            elif choice in {'n', 'N', 'no', 'No'}:
+                choice = input('\nThe current default packet number is 100, would you like to change it? (y,n): ')
+                if choice in {'y', 'Y', 'yes', 'Yes'}:
+                    packetNumber = integerInput('Enter the amount of packets to be sniffed: ')
+                    summarizedProtocolPacket(interfaceName, packetNumber, protocol)
+                else:
+                    packetNumber = 100
+                    summarizedProtocolPacket(interfaceName, packetNumber, protocol)
+                break
+            # Invalid option
+            else:
+                print('Invalid option\n')
+
+def menuLoop():
     # Main Menu
     while True:
         # Show menu options
@@ -87,40 +171,27 @@ if __name__ == '__main__':
 
         # All sniffed packets
         if choice == 1:
-            while True:
-                choice = input('\nDo you want to see all the information about each packet? (y,n): ')
-                # All the information
-                if choice in {'y', 'Y', 'yes', 'Yes'}:
-                    choice = input('\nThe current default packet number is 100, would you like to change it? (y,n): ')
-                    if choice in {'y', 'Y', 'yes', 'Yes'}:
-                        packetNumber = integerInput('Enter the amount of packets to be sniffed: ')
-                        detailedPacket(interfaceName, packetNumber)
-                    else:
-                        packetNumber = 100
-                        detailedPacket(interfaceName, packetNumber)
-                    break
-                # Summarized information
-                elif choice in {'n', 'N', 'no', 'No'}:
-                    choice = input('\nThe current default packet number is 100, would you like to change it? (y,n): ')
-                    if choice in {'y', 'Y', 'yes', 'Yes'}:
-                        packetNumber = integerInput('Enter the amount of packets to be sniffed: ')
-                        summarizedPacket(interfaceName, packetNumber)
-                    else:
-                        packetNumber = 100
-                        summarizedPacket(interfaceName, packetNumber)
-                    break
-                # Invalid option
-                else:
-                    print('Invalid option\n')
+            sniffedPackets()
 
-        # Packets of a specific layer
+        # Packets of a specific protocol
         elif choice == 2:
-            print("Hello")
+            protocolPackets()
 
         # Finalize program
-        elif choice == 4:
+        elif choice == 3:
             print('Thanks for using the program! Have a good day/night (^-^)')
             sys.exit()
 
         else:
             print('Not a valid option \n')
+
+# Main
+if __name__ == '__main__':
+    # Welcome happy message :)
+    print('---Welcome to the Python Sniffer program---\n')
+    
+    # Ask for the interface name
+    interfaceName = inputInterfaceName()
+
+    # Loop through the menu
+    menuLoop()
